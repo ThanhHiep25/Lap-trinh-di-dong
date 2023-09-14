7ad77317328bb76633bedbf927af7ddb97d58743
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-09-14 11:48:12 +0700

    Chưa hoàn thành

---
+++ STT_10NguyenThanhHiep_Tuan02/JS/DOM.js
@@ -0,0 +1,30 @@
+//QuerySelector () dùng để lựa chọn các phần tử
+// Phương thức này trả về một đối tượng có id đã chọn
+
+const number = document.querySelector("#number");
+const message = document.querySelector("#message");
+const scoreElement = document.querySelector("#score");
+const guessInput = document.querySelector("#guess");
+const againButton = document.querySelector(".again");
+
+const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+function resetGame() {
+  // Khôi phục giá trị ban đầu của biến 'score' và 'secretNumber'
+  const score = 0;
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+  // Khôi phục các điều kiện ban đầu của các trường nhập tin nhắn, số, điểm và đoán
+  message.textContent = "";
+  number.textContent = "";
+  scoreElement.textContent = score;
+  guessInput.value = "";
+  // khôi phục màu nền ban đầu (#222) và chiều rộng số (15rem)
+  number.setAttribute("style", "backgroundColor: #222; width: 15rem;");
+
+  const secretNumberInt = parseInt(secretNumber, 10);
+  number.textContent = secretNumberInt;
+}
+
+if (againButton) {
+  againButton.addEventListener("click", resetGame);
+}
