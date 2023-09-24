b1a12506430b97bc46553ee4b7cabc976256af4a
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-09-24 12:01:27 +0700

    new update tuan2

--- STT_10NguyenThanhHiep_Tuan02/JS/DOM.js
+++ STT_10NguyenThanhHiep_Tuan02/JS/DOM.js
@@ -1,31 +1,20 @@
 //QuerySelector () dùng để lựa chọn các phần tử
 // Phương thức này trả về một đối tượng có id đã chọn
 
-const number = document.querySelector("#number");
-const message = document.querySelector("#message");
-const scoreElement = document.querySelector("#score");
-const guessInput = document.querySelector("#guess");
-const againButton = document.querySelector(".again");
+const againButton = document.querySelector('.again');
 
-const secretNumber = Math.floor(Math.random() * 100) + 1;
-
-function resetGame() {
-  // Khôi phục giá trị ban đầu của biến 'score' và 'secretNumber'
-  const score = 0;
-  let secretNumber;
+againButton.addEventListener('click', () => {
+  // Restore the initial values of the score and secretNumber variables.
+  score = 0;
   secretNumber = Math.floor(Math.random() * 100) + 1;
-  // Khôi phục các điều kiện ban đầu của các trường nhập tin nhắn, số, điểm và đoán
-  message.textContent = "";
-  number.textContent = "";
-  scoreElement.textContent = score;
-  guessInput.value = "";
-  // khôi phục màu nền ban đầu (#222) và chiều rộng số (15rem)
-  number.setAttribute("style", "backgroundColor: #222; width: 15rem;");
+  
+  // Restore the initial conditions of the message, number, score, and guess input fields.
+  messageElement.innerHTML = 'Make a guess!';
+  numberElement.innerHTML = secretNumber;
+  scoreElement.innerHTML = score;
+  guessInputElement.value = '';
 
-  const secretNumberInt = parseInt(secretNumber, 10);
-  number.textContent = secretNumberInt;
-}
-
-if (againButton) {
-  againButton.addEventListener("click", resetGame);
-}
+  // Restore the original background color and number width.
+  document.body.style.backgroundColor = '#222';
+  numberElement.style.width = '15rem';
+});
