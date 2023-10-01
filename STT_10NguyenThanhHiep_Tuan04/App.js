2495ea8de11d847853c00685121468af8cb4407e
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-10-01 13:53:56 +0700

    + mới

--- STT_10NguyenThanhHiep_Tuan04/App.js
+++ STT_10NguyenThanhHiep_Tuan04/App.js
@@ -3,14 +3,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./App/Screens/Home";
 import Home2 from "./App/Screens/Home2";
-import Screens3 from './App/Screens/Screens3';
-import Login from './App/Screens/Login';
+import Screens3 from "./App/Screens/Screens3";
+import Login from "./App/Screens/Login";
+import XmEye from "./App/Screens/XmEye";
 
 export default function App() {
   return (
     //<Home />,
     //<Home2 />,
     //<Screens3/>,
-    <Login/>
+    //(<Login />),
+     (<XmEye />)
   );
 }
