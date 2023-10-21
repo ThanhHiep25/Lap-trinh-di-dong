9f842fd3bd2ab1887c4348d071ba1718cc897687
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-10-21 19:53:58 +0700

    create navigation

---
+++ STT_10NguyenThanhHiep_Tuan07_2/src/navigation/Appnavigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNavigativeStackNavigator();
+export const Appnavigation = () => {
+    return (
+       
+    )
+}
+
