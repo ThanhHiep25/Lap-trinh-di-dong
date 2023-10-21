803fc0d3ce035a109e658754661bd090fa85c2ca
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-10-21 20:37:08 +0700

    create data

--- STT_10NguyenThanhHiep_Tuan07_2/src/navigation/Appnavigation.js
+++ STT_10NguyenThanhHiep_Tuan07_2/src/navigation/Appnavigation.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { Home } from '../screens/home';
+import { Modal } from '../screens/modal';
 
-
-const Stack = createNavigativeStackNavigator();
+const Stack = createNativeStackNavigator();
 export const Appnavigation = () => {
     return (
-       
+       <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: true}}>
+            <Stack.Screen name='Home' component={Home}/>
+            <Stack.Screen name='Modal' component={Modal}/>
+        </Stack.Navigator>
+       </NavigationContainer>
     )
 }
 
---
+++ STT_10NguyenThanhHiep_Tuan07_2/src/screens/modal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export const Modal = () => {
+    return (
+        <View>
+            <Text>Modal</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
