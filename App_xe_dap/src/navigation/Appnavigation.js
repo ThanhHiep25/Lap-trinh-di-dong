f6596b5123b41028de01b2ac75b632e5097333b5
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-10-25 20:52:04 +0700

    Create Welcome bike

--- App_xe_dap/src/navigation/Appnavigation.js
+++ App_xe_dap/src/navigation/Appnavigation.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import {  } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Welcome from "../screens/Welcome";
+import { BikeList } from "../screens/BikeList";
+
+const Stack = createNativeStackNavigator();
+const Appnavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Bike" component={BikeList} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Appnavigation;
---
+++ App_xe_dap/src/screens/BikeList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, View, Text } from 'react-native'
+
+export const BikeList = () => {
+    return (
+        <View>
+            <Text>BikeList</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
