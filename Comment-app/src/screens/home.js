9e09ccfe5e779f3a48909dc26e93c39e0fdd3794
Nguyenthanhhiep <87129597+ThanhHiep25@users.noreply.github.com>
2023-11-09 18:51:05 +0700

    xong login

---
+++ Comment-app/src/screens/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, View, Text } from 'react-native'
+
+export const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
--- Comment-app/src/screens/login.js
+++ Comment-app/src/screens/login.js
@@ -1,7 +1,19 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  Pressable,
+} from "react-native";
 
 export const Login = () => {
+
+     const navigation = useNavigation();
+     const route = useRoute();
+     
   return (
     <View style={styles.container}>
       <View style={styles.view}>
@@ -10,8 +22,24 @@ export const Login = () => {
           style={styles.img}
         />
       </View>
+
       <View style={styles.view1}>
         <Text style={styles.text}>Login</Text>
+      </View>
+
+      <View style={styles.view2}>
+        <TextInput style={styles.textIn} placeholder="Email or User name" />
+        <TextInput style={styles.textIn} placeholder="Password" />
+      </View>
+
+      <View style={styles.view3}>
+        <Pressable style={styles.Pre}>
+          <Text style={styles.text1}>Sumbmit</Text>
+          <Image
+            source={require("../../assets/IMG/sub.png")}
+            style={styles.img1}
+          />
+        </Pressable>
       </View>
     </View>
   );
@@ -25,21 +53,66 @@ const styles = StyleSheet.create({
   view: {
     alignItems: "center",
     margin: 20,
+    marginTop: 50,
   },
   view1: {
     alignItems: "center",
     margin: 20,
+  },
+  view2: {
+    alignItems: "center",
+    margin: 20,
+  },
+  view3: {
+    alignItems: "center",
+    margin: 20,
+    marginTop: 50,
   },
   img: {
     height: 180,
     width: 180,
     resizeMode: "contain",
   },
-
+  img1: {
+    height: 30,
+    width: 30,
+    resizeMode: "contain",
+  },
   text: {
     fontSize: 40,
     fontWeight: 700,
     color: "#ffff",
     fontFamily: "Arial",
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: 700,
+    color: "#ffff",
+    fontFamily: "Arial",
+  },
+  textIn: {
+    fontSize: 20,
+    fontWeight: 500,
+    color: "#ffff",
+    fontFamily: "Arial",
+    height: 50,
+    width: 330,
+    marginTop: 20,
+    backgroundColor: "#D9D9D978",
+    color: "#E9A0A0",
+    padding: 10,
+    paddingLeft: 50,
+    borderRadius: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
+  },
+  Pre: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    height: 50,
+    width: 250,
+    backgroundColor: "#61BDF1",
+    borderRadius: 20,
   },
 });
